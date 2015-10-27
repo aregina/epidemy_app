8ced23062e8e22d5ddeb69058a832d96f14842f4
Regina Arslanova <aregina@yandex.ru>
2015-10-27 23:24:36 +0500

    pr comments was fixed

--- epidemy_mock/views.py
+++ epidemy_mock/views.py
@@ -1,15 +1,15 @@
+import datetime
 from rest_framework import viewsets
 from .models import YupeConcerts, YupeNews
 from .serializers import ConcertsSerializer, NewsSerializer
-import datetime
 
 
 class ConcertsViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
     """
-    two_days_ago = datetime.date.today() - datetime.timedelta(days=1)
-    queryset = YupeConcerts.objects.filter(date__gte=two_days_ago).order_by('date')
+    one_day_ago = datetime.date.today() - datetime.timedelta(days=1)
+    queryset = YupeConcerts.objects.filter(date__gte=one_day_ago).order_by('date')
     serializer_class = ConcertsSerializer
 
 
